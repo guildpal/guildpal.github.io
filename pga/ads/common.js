81de8696b54ff3e64d0837ba733bd679eeaf6768
Jimmy <jimmy@atomrigs.io>
2024-07-11 14:49:32 +0900

    + Show Persona 4 times and Cointraffic 1 time

---
+++ pga/ads/common.js
@@ -0,0 +1,47 @@
+let adClient = null;
+let adUnitConfig = {};
+
+function setupPersona(adUnitId) {
+  var personaConfig = {
+    apiKey: "persona-pub-0x3206daf2b97d0bd17bf8e10ead94dee5", // An actual API key is generated once you register an app with us.
+    environment: "production", // use value 'production' when going live
+  }
+  var sdk = new PersonaAdSDK.PersonaAdSDK(personaConfig);
+  adClient = sdk.getClient();
+
+  adUnitConfig = {
+    adUnitId,
+    containerId: "pga-banner-ad"
+  }
+}
+
+const adPeriod = 20000;
+function showPersonaAd() {
+  if (adClient === null) {
+    return;
+  }
+
+  adClient.showBannerAd(adUnitConfig, (errorMessage) => {
+    console.log('Persona error:', errorMessage);
+    location.href = 'https://guildpal.com/pga/ads/pga-ads-cointraffic.html?rotation=self';
+  });
+
+  const searchParams = new URLSearchParams(window.location.search);
+  const orgDisplayCount = searchParams.get('displayCount');
+  const displayCount = orgDisplayCount ? Number(orgDisplayCount) : 1;
+  setTimeout(() => {
+    if (displayCount <= 4) {
+      const { origin, pathname } = window.location;
+      const url = `${origin}${pathname}?displayCount=${displayCount + 1}`;
+      // console.log('show persona!', url);
+      location.href = url;
+    } else {
+      console.log('Lets go to Cointraffic');
+      location.href = 'https://guildpal.com/pga/ads/pga-ads-cointraffic.html';
+    }
+  }, adPeriod);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  showPersonaAd();
+})
