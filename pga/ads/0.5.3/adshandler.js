260e36ee4325f3024b505971af104380531aa2ee
TS Kang <ts.kang@atomrigs.io>
2024-07-30 19:03:08 +0900

    test ads impression, postclick (0.5.3)

--- pga/ads/0.5.3/adshandler.js
+++ pga/ads/0.5.3/adshandler.js
@@ -37,13 +37,13 @@ const pgaAdsConfigs = `
     "personaUnitId": "e7b6f005-3d79-4e74-bf6d-6729f33262a1"
   },
   "market": {
-    "rotation": false,
-    "allocation": ["smartyads"],
+    "rotation": true,
+    "allocation": ["ads", "pga", "pga"],
     "adRotationPeriod": 30,
     "personaUnitId": "fe24a1b0-9d34-4cd4-ab42-aeaf5836f594"
   }
 }
-`
+`;
 const defaultPersonaAdUnitId = "d126cd27-d130-425e-a332-6b33a0b947b4"; // home
 const timeSecond = 1000;
 
@@ -51,16 +51,95 @@ const timeSecond = 1000;
 
 let pgaAdConfig = {};
 let personaAdUnitId = defaultPersonaAdUnitId;
+let index = 0;
+let slot = "home";
+let playerId = "";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const search = removeTabParameterFromUrl(window.location.search);
+  console.log("search:", search);
+
+  const searchParams = new URLSearchParams(search);
+  index = Number(searchParams.get("index")) || 0;
+  slot = (searchParams.get("slot") || "home").toLowerCase().trim();
+  playerId = searchParams.get("mid");
+
+  pgaAdConfig = JSON.parse(pgaAdsConfigs)[slot];
+  personaAdUnitId = pgaAdConfig.personaUnitId;
+
+  const body = document.querySelector("body");
+  body.addEventListener("click", processClick);
+
+  showAd(slot, index);
+});
+
+async function processClick(e) {
+  try {
+    const response = await fetch(
+      "https://pga-svc-ads.play-extension.xyz/ads-api/addinteraction",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "X-Atomrigs-Pga-Pid": playerId,
+        },
+        body: JSON.stringify({
+          player_id: playerId,
+          domain: "display",
+          subject: pgaAdConfig?.allocation?.[index] || "",
+          slot: `pga/${slot}`,
+          ts: new Date().getTime(),
+        }),
+      }
+    );
+    const result = await response.json();
+    if (result.showResult) {
+      alert(result.resultMessage);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function processImpression() {
+  try {
+    const response = await fetch(
+      "https://pga-svc-ads.play-extension.xyz/ads-api/addimpression",
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "X-Atomrigs-Pga-Pid": playerId,
+        },
+        body: JSON.stringify({
+          player_id: playerId,
+          domain: "display",
+          subject: pgaAdConfig?.allocation?.[index] || "",
+          slot: `pga/${slot}`,
+          ts: new Date().getTime(),
+        }),
+      }
+    );
+    const result = await response.json();
+    console.log("processImpression", result);
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 function showAd(slot, index) {
+  processImpression();
+
   if (index < pgaAdConfig.allocation.length) {
     switch (pgaAdConfig.allocation[index]) {
       case "persona":
         showPersona(pgaAdConfig.personaUnitId, slot, index);
         break;
       case "cointraffic":
-          showCointraffic(slot, index);
-          break;
+        showCointraffic(slot, index);
+        break;
       case "pga":
         showPGA(slot, index);
         break;
@@ -81,41 +160,31 @@ function showAd(slot, index) {
     setTimeout(() => {
       showAd(slot, index);
     }, pgaAdConfig.adRotationPeriod * timeSecond);
-  } else if (index === pgaAdConfig.allocation.length && pgaAdConfig.rotation === true) {
-      setTimeout(() => {
-        showAd(slot, 0);
-      }, pgaAdConfig.adRotationPeriod * timeSecond);
+  } else if (
+    index === pgaAdConfig.allocation.length &&
+    pgaAdConfig.rotation === true
+  ) {
+    setTimeout(() => {
+      showAd(slot, 0);
+    }, pgaAdConfig.adRotationPeriod * timeSecond);
   }
 }
 
 function removeTabParameterFromUrl(search) {
-  const paramStart = search.indexOf('?tab=');
+  const paramStart = search.indexOf("?tab=");
   if (paramStart !== -1) {
-    const paramEnd = search.indexOf('&', paramStart);
+    const paramEnd = search.indexOf("&", paramStart);
     if (paramEnd !== -1) {
       // Remove the entire parameter including its value
-      return search.substring(0, paramStart) + '&' + search.substring(paramEnd + 1);
+      return (
+        search.substring(0, paramStart) + "&" + search.substring(paramEnd + 1)
+      );
     } else {
       return search.substring(0, paramStart);
     }
   }
   return search;
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-  const search = removeTabParameterFromUrl(window.location.search);
-  console.log('search:', search);
-
-  const searchParams = new URLSearchParams(search);
-  const slot = (searchParams.get("slot") || "home").toLowerCase().trim();
-  const index = Number(searchParams.get("index")) || 0;
-
-  pgaAdConfig = JSON.parse(pgaAdsConfigs)[slot];
-  personaAdUnitId = pgaAdConfig.personaUnitId;
-
-  showAd(slot, index);
-});
-
 
 ///////////////////////// agency codes
 
@@ -127,9 +196,8 @@ const PERSONA_SDK_CONFIG = {
 };
 
 function showPersona(adUnitId, slot, index) {
-
-  let containerDiv = document.querySelector('body > div');
-  containerDiv.innerHTML = '';
+  let containerDiv = document.querySelector("body > div");
+  containerDiv.innerHTML = "";
 
   let adUnitConfig = {
     adUnitId,
@@ -140,7 +208,7 @@ function showPersona(adUnitId, slot, index) {
   const adClient = sdk.getClient();
   if (adClient === null) {
     console.log("Persona error: adClient is null..");
-    return
+    return;
   }
 
   adClient.showBannerAd(adUnitConfig, (errorMessage) => {
@@ -155,16 +223,16 @@ function showPersona(adUnitId, slot, index) {
 // coinTraffic
 
 function showCointraffic(slot, index) {
+  let containerDiv = document.querySelector("body > div");
+  containerDiv.innerHTML = "";
 
-  let containerDiv = document.querySelector('body > div');
-  containerDiv.innerHTML = '';
+  let spanElement = document.createElement("span");
+  spanElement.id = "ct_cn9L6gxT7Hq";
 
-  let spanElement = document.createElement('span');
-  spanElement.id = 'ct_cn9L6gxT7Hq';
-
-  let scriptElement = document.createElement('script');
+  let scriptElement = document.createElement("script");
   scriptElement.async = true;
-  scriptElement.src = 'https://appsha-prm.ctengine.io/js/script.js?wkey=bkz3FU91fH';
+  scriptElement.src =
+    "https://appsha-prm.ctengine.io/js/script.js?wkey=bkz3FU91fH";
 
   containerDiv.appendChild(spanElement);
   containerDiv.appendChild(scriptElement);
@@ -182,53 +250,58 @@ function showCointraffic(slot, index) {
 // pga
 
 function showPGA(slot, index) {
+  let containerDiv = document.querySelector("body > div");
+  containerDiv.innerHTML = "";
 
-  let containerDiv = document.querySelector('body > div');
-  containerDiv.innerHTML = '';
+  let anchorElement = document.createElement("a");
+  anchorElement.href = "https://pixels.guildpal.com";
+  anchorElement.target = "_blank";
 
-  let anchorElement = document.createElement('a');
-  anchorElement.href = 'https://pixels.guildpal.com';
-  anchorElement.target = '_blank';
-
-  let imgElement = document.createElement('img');
-  if (index%2 === 0) {
-    imgElement.src = './images/pga-ad-guildpalpage-2_240726.gif';
+  let imgElement = document.createElement("img");
+  if (index % 2 === 0) {
+    imgElement.src = "./images/pga-ad-guildpalpage-2_240726.gif";
   } else {
-    imgElement.src = './images/pga-ad-guildpalpage-1_240726.gif';
+    imgElement.src = "./images/pga-ad-guildpalpage-1_240726.gif";
   }
   imgElement.width = 320;
   imgElement.height = 100;
-  imgElement.alt = 'pixels.guildpal.com';
+  imgElement.alt = "pixels.guildpal.com";
 
   anchorElement.appendChild(imgElement);
   containerDiv.appendChild(anchorElement);
 }
 
-
 // ads
 
 function showADS(slot, index) {
+  let containerDiv = document.querySelector("body > div");
+  containerDiv.innerHTML = "";
 
-  let containerDiv = document.querySelector('body > div');
-  containerDiv.innerHTML = '';
-
-  let iframeElement = document.createElement('iframe');
-  iframeElement.setAttribute('data-aa', '2337944');
-  iframeElement.src = '//ad.a-ads.com/2337944?size=320x100';
-  iframeElement.style.width = '320px';
-  iframeElement.style.height = '100px';
-  iframeElement.style.border = '0px';
-  iframeElement.style.padding = '0';
-  iframeElement.style.overflow = 'hidden';
-  iframeElement.style.backgroundColor = 'transparent';
+  let iframeElement = document.createElement("iframe");
+  iframeElement.setAttribute("data-aa", "2337944");
+  iframeElement.src = "//ad.a-ads.com/2337944?size=320x100";
+  iframeElement.style.width = "320px";
+  iframeElement.style.height = "100px";
+  iframeElement.style.border = "0px";
+  iframeElement.style.padding = "0";
+  iframeElement.style.overflow = "hidden";
+  iframeElement.style.backgroundColor = "transparent";
 
   containerDiv.appendChild(iframeElement);
 }
 
 function showSmartyAds(slot, index) {
-  let containerDiv = document.querySelector('body > div');
-  containerDiv.innerHTML = '';
+  let containerDiv = document.querySelector("body > div");
+  containerDiv.innerHTML = "";
 
-  var adUnits = [ { code: 'pga-banner-ad', placement_id: 4203, sizes: [320, 100], refreshable: true, refreshIntervalSec: 30 } ];
+  var adUnits = [
+    {
+      code: "pga-banner-ad",
+      placement_id: 4203,
+      sizes: [320, 100],
+      refreshable: true,
+      refreshIntervalSec: 30,
+    },
+  ];
   smarty.buildUnits(adUnits);
 }
