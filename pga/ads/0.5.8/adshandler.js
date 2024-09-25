aa0b9090edc459d6ae1c1b45404b40a9be132cb2
lcs86 <lcs86@atomrigs.io>
2024-09-25 10:25:57 +0900

    show `cointraffic` in guild tab again
    * prebid does not show ad as much as direct cointraffic sdk integration

--- pga/ads/0.5.8/adshandler.js
+++ pga/ads/0.5.8/adshandler.js
@@ -99,7 +99,7 @@ const pgaAdsConfigs = {
   },
   guild: {
     rotation: false,
-    allocation: [ADS.prebid],
+    allocation: [ADS.cointraffic],
     adRotationPeriod: 30,
     personaUnitId: "e7b6f005-3d79-4e74-bf6d-6729f33262a1",
   },
