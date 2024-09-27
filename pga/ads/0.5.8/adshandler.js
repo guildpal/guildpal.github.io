3eeb5619ecdfe0c92c87e6dc709d8289c632903a
Luke-Atomrigs <k@atomrigs.io>
2024-09-27 09:53:34 +0900

    lootrush ads removed

--- pga/ads/0.5.8/adshandler.js
+++ pga/ads/0.5.8/adshandler.js
@@ -63,7 +63,7 @@ const ADS = {
 const pgaAdsConfigs = {
   home: {
     rotation: false,
-    allocation: [ADS.lootrush],
+    allocation: [ADS.hypelab],
     adRotationPeriod: 30,
     personaUnitId: "d126cd27-d130-425e-a332-6b33a0b947b4",
   },
@@ -87,7 +87,7 @@ const pgaAdsConfigs = {
   },
   storage: {
     rotation: false,
-    allocation: [ADS.lootrush],
+    allocation: [ADS.hypelab],
     adRotationPeriod: 30,
     personaUnitId: "157d8bb8-eb2b-443e-80f0-1f2a5977a4c4",
   },
@@ -105,7 +105,7 @@ const pgaAdsConfigs = {
   },
   market: {
     rotation: false,
-    allocation: [ADS.lootrush],
+    allocation: [ADS.hypelab],
     adRotationPeriod: 30,
     personaUnitId: "fe24a1b0-9d34-4cd4-ab42-aeaf5836f594",
   },
@@ -121,7 +121,7 @@ const allAdsSubject = "ALL-ADS";
 const pgaSelfAdsSubject = "pga";
 const prebidAdUnits = [
   {
-    code: 'pga-banner-ad',
+    code: "pga-banner-ad",
     mediaTypes: {
       banner: {
         sizes: [[320, 100]],
@@ -129,9 +129,9 @@ const prebidAdUnits = [
     },
     bids: [
       {
-        bidder: 'cointraffic',
+        bidder: "cointraffic",
         params: {
-          placementId: 'cn9L6gxT7Hq', // Banner Code in dashboard
+          placementId: "cn9L6gxT7Hq", // Banner Code in dashboard
         },
       },
     ],
@@ -440,65 +440,72 @@ function showPrebid(slot, index) {
   const containerDiv = document.querySelector("div#pga-banner-ad");
   containerDiv.innerHTML = "";
 
-  pbjs.onEvent('bidWon', (data) => {
-    console.log(data.bidderCode + ' won the ad server auction for ad unit ' + data.adUnitCode + ' at ' + data.cpm + ' CPM');
+  pbjs.onEvent("bidWon", (data) => {
+    console.log(
+      data.bidderCode +
+        " won the ad server auction for ad unit " +
+        data.adUnitCode +
+        " at " +
+        data.cpm +
+        " CPM"
+    );
     processImpression(domainDisplay, "agent/prebid", slot);
   });
-  pbjs.onEvent('bidRejected', (data) => {
-    console.log('prebid adRenderFailed', data);
+  pbjs.onEvent("bidRejected", (data) => {
+    console.log("prebid adRenderFailed", data);
     showADS();
   });
-  pbjs.onEvent('adRenderFailed', (data) => {
-    console.log("prebid bidRejected", data)
+  pbjs.onEvent("adRenderFailed", (data) => {
+    console.log("prebid bidRejected", data);
   });
-  pbjs.onEvent('bidTimeout', (data) => {
-    console.log("prebid timeout", data)
+  pbjs.onEvent("bidTimeout", (data) => {
+    console.log("prebid timeout", data);
   });
 
   pbjs.removeAdUnit();
 
   pbjs.que.push(function () {
-    pbjs.addAdUnits(prebidAdUnits)
+    pbjs.addAdUnits(prebidAdUnits);
     pbjs.requestBids({
       timeout: 2000,
       bidsBackHandler: renderAllAdUnits,
-    })
-  })
+    });
+  });
 }
 function renderAllAdUnits() {
-  var winners = pbjs.getHighestCpmBids()
+  var winners = pbjs.getHighestCpmBids();
   for (var i = 0; i < winners.length; i++) {
-    renderOne(winners[i])
+    renderOne(winners[i]);
   }
 }
 function renderOne(winningBid) {
   if (winningBid && winningBid.adId) {
-    var div = document.getElementById(winningBid.adUnitCode)
+    var div = document.getElementById(winningBid.adUnitCode);
     if (div) {
-      const iframe = document.createElement('iframe');
-      iframe.scrolling = 'no';
-      iframe.frameBorder = '0';
-      iframe.marginHeight = '0';
-      iframe.marginHeight = '0';
+      const iframe = document.createElement("iframe");
+      iframe.scrolling = "no";
+      iframe.frameBorder = "0";
+      iframe.marginHeight = "0";
+      iframe.marginHeight = "0";
       iframe.name = `prebid_ads_iframe_${winningBid.adUnitCode}`;
-      iframe.title = '3rd party ad content';
+      iframe.title = "3rd party ad content";
       iframe.sandbox.add(
-        'allow-forms',
-        'allow-popups',
-        'allow-popups-to-escape-sandbox',
-        'allow-same-origin',
-        'allow-scripts',
-        'allow-top-navigation-by-user-activation'
+        "allow-forms",
+        "allow-popups",
+        "allow-popups-to-escape-sandbox",
+        "allow-same-origin",
+        "allow-scripts",
+        "allow-top-navigation-by-user-activation"
       );
-      iframe.setAttribute('aria-label', 'Advertisment');
-      iframe.style.setProperty('border', '0');
-      iframe.style.setProperty('margin', '0');
-      iframe.style.setProperty('overflow', 'hidden');
+      iframe.setAttribute("aria-label", "Advertisment");
+      iframe.style.setProperty("border", "0");
+      iframe.style.setProperty("margin", "0");
+      iframe.style.setProperty("overflow", "hidden");
       div.appendChild(iframe);
       const iframeDoc = iframe.contentWindow.document;
       pbjs.renderAd(iframeDoc, winningBid.adId);
       const normalizeCss = `/*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */button,hr,input{overflow:visible}progress,sub,sup{vertical-align:baseline}[type=checkbox],[type=radio],legend{box-sizing:border-box;padding:0}html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}details,main{display:block}h1{font-size:2em;margin:.67em 0}hr{box-sizing:content-box;height:0}code,kbd,pre,samp{font-family:monospace,monospace;font-size:1em}a{background-color:transparent}abbr[title]{border-bottom:none;text-decoration:underline;text-decoration:underline dotted}b,strong{font-weight:bolder}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative}sub{bottom:-.25em}sup{top:-.5em}img{border-style:none}button,input,optgroup,select,textarea{font-family:inherit;font-size:100%;line-height:1.15;margin:0}button,select{text-transform:none}[type=button],[type=reset],[type=submit],button{-webkit-appearance:button}[type=button]::-moz-focus-inner,[type=reset]::-moz-focus-inner,[type=submit]::-moz-focus-inner,button::-moz-focus-inner{border-style:none;padding:0}[type=button]:-moz-focusring,[type=reset]:-moz-focusring,[type=submit]:-moz-focusring,button:-moz-focusring{outline:ButtonText dotted 1px}fieldset{padding:.35em .75em .625em}legend{color:inherit;display:table;max-width:100%;white-space:normal}textarea{overflow:auto}[type=number]::-webkit-inner-spin-button,[type=number]::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}[type=search]::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}summary{display:list-item}[hidden],template{display:none}`;
-      const iframeStyle = iframeDoc.createElement('style');
+      const iframeStyle = iframeDoc.createElement("style");
       iframeStyle.appendChild(iframeDoc.createTextNode(normalizeCss));
       iframeDoc.head.appendChild(iframeStyle);
     }
